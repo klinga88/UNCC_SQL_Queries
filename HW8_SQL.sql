887ac3cb6d8a174b405b8efff2cd111b77852afe
klinga <klinga88@gmail.com>
2018-07-23 16:55:00 -0400

    Completed for submission

--- HW8_SQL.sql
+++ HW8_SQL.sql
@@ -88,7 +88,76 @@ ORDER BY c.last_name;
 SELECT f.title
 FROM film f
 INNER JOIN language l ON f.language_id = l.language_id
-WHERE f.title IN(
+WHERE l.name = 'English' AND 
+f.title IN(
 SELECT f.title
 FROM film f
 WHERE (f.title LIKE 'K%' OR f.title LIKE 'Q%'));
+-- 7b
+SELECT a.first_name, a.last_name
+FROM actor a
+INNER JOIN film_actor fa on a.actor_id = fa.actor_id
+WHERE fa.film_id IN(
+SELECT f.film_id
+FROM film f
+WHERE f.title = 'Alone Trip');
+-- 7c 
+SELECT c.first_name,c.last_name,c.email 
+FROM customer c
+INNER JOIN address a ON c.address_id = a.address_id
+INNER JOIN city ci ON ci.city_id = a.city_id
+INNER JOIN country co ON co.country_id = ci.country_id
+WHERE co.country = 'Canada';
+-- 7d
+SELECT f.title
+FROM film f
+INNER JOIN film_category fc ON fc.film_id = f.film_id
+INNER JOIN category c ON fc.category_id = c.category_id
+WHERE c.name = 'Family';
+-- 7e
+SELECT f.title, count(f.title) 'Times Rented'
+FROM film f
+INNER JOIN inventory i ON f.film_id = i.film_id
+INNER JOIN rental r ON r.inventory_id = i.inventory_id
+GROUP BY f.title
+ORDER BY count(f.title) DESC;
+-- 7f
+SELECT s.store_id, sum(p.amount) 'Total Revenue'
+FROM payment p
+INNER JOIN staff st ON p.staff_id = st.staff_id
+INNER JOIN store s ON s.store_id = st.store_id
+GROUP BY s.store_id;
+-- 7g
+SELECT s.store_id, c.city, co.country
+FROM store s
+INNER JOIN address a ON s.address_id = a.address_id
+INNER JOIN city c ON c.city_id = a.city_id
+INNER JOIN country co ON co.country_id = c.country_id;
+-- 7h
+SELECT c.name 'Film Category', sum(p.amount) 'Total Revenue' 
+FROM category c
+INNER JOIN film_category fc ON fc.category_id = c.category_id
+INNER JOIN film f ON f.film_id = fc.film_id
+INNER JOIN inventory i ON i.film_id = f.film_id
+INNER JOIN rental r ON r.inventory_id = i.inventory_id
+INNER JOIN payment p ON p.rental_id = r.rental_id
+GROUP BY c.name
+ORDER BY sum(p.amount) DESC
+LIMIT 5;
+
+-- 8a
+CREATE VIEW revenue_by_category AS
+SELECT c.name 'Film Category', sum(p.amount) 'Total Revenue' 
+FROM category c
+INNER JOIN film_category fc ON fc.category_id = c.category_id
+INNER JOIN film f ON f.film_id = fc.film_id
+INNER JOIN inventory i ON i.film_id = f.film_id
+INNER JOIN rental r ON r.inventory_id = i.inventory_id
+INNER JOIN payment p ON p.rental_id = r.rental_id
+GROUP BY c.name
+ORDER BY sum(p.amount) DESC
+LIMIT 5;
+-- 8b
+SELECT * FROM revenue_by_category;
+-- 8c
+DROP VIEW revenue_by_category;
